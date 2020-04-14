92c9ba8b686b1005845f07b6c83b94eb70d42386
joelmswearingen <60490297+joelmswearingen@users.noreply.github.com>
2020-04-13 22:51:08 -0500

    Create would-you-rather

---
+++ would-you-rather/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <h1>Would you rather...</h1>  
+
+    <WouldYouRatherQuestion 
+      v-for="question in questions"
+      v-bind:key="question.id"
+      v-bind:question="question"
+      v-bind:choice="choice"
+      v-on:choice-selected="userAnswerSelected"
+    ></WouldYouRatherQuestion>
+
+    <h2>You Would Rather...</h2>  
+    <div>
+        {{ userSelectionMessage }}            
+    </div>
+  </div>
+</template>
+
+<script>
+import WouldYouRatherQuestion from './components/WouldYouRatherQuestion.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WouldYouRatherQuestion
+  },
+  data() {
+    return {
+      questions: [
+        {
+          id: 0,
+          question: '...be a reverse centaur or a reverse mermaid/merman?',
+          answer1: 'reverse centaur',
+          answer2: 'reverse mermaid/merman',
+        },
+        {
+          id: 1,
+          question: '...be raised by monkeys or by wolves?',
+          answer1: 'monkeys',
+          answer2: 'wolves',          
+        },
+        {
+          id: 2,
+          question: '...have hooves for feet or webbed fingers?',
+          answer1: 'hooves for feet',
+          answer2: 'webbed fingers',          
+        },
+      ],
+      answers: [],
+      userSelectionMessage: '',
+    }
+  },
+  method: {
+    userAnswerSelected(choice) {
+      this.answers.push(choice)
+    },
+
+  },
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ would-you-rather/src/components/WouldYouRatherQuestion.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <h2>{{ question.question }}</h2>
+
+    <input type="radio" v-model="choice" v-bind:value="answer1" v-on:click="userAnswerSelected(choice)">
+    <label>{{ question.answer1 }}</label>
+    <input type="radio" v-model="choice" v-bind:value="answer2" v-on:click="userAnswerSelected(choice)">
+    <label>{{ question.answer2 }}</label>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WouldYouRatherQuestion',
+  data() {
+    return {
+      choice: ''
+    }
+  }, 
+  props: {
+    id: Number,
+    question: Object,
+    answer1: String,
+    answer2: String,
+    answers: Array,
+  },
+  methods: {
+    userAnswerSelected(choice) {
+      this.$emit('choice-selected', choice)
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
