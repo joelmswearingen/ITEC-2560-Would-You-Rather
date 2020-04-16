bf4c7fb0beb5e010e1fe4aa52cc80bc25dfcdda8
joelmswearingen <60490297+joelmswearingen@users.noreply.github.com>
2020-04-16 18:29:32 -0500

    Second Push

--- would-you-rather/src/App.vue
+++ would-you-rather/src/App.vue
@@ -57,16 +57,6 @@ export default {
         let index = choice.id
         this.choices[index] = choice.choice
         this.$set(this.choices, index, choice.choice)
-
-        // this.choices.push(choice)  
-        
-        // this.choices.forEach(element => {
-        //   if element.id = choice.id
-          
-        // });
-        // // how to replace existing object where key exists?
-        // this.choices.sort(function(c1, c2) {
-        //   return c1.id > c2.id ? 1 : -1 
         }
     },
 }
